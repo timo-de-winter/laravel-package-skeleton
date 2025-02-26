75a094369fcdea5b9d5aa821a458a02ede0ff72a
Timo de Winter <timo@be-interactive.nl>
2025-02-26 11:28:31 +0100

    Initial Commit

---
+++ src/SkeletonServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace VendorName\Skeleton;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class SkeletonServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('skeleton')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_migration_table_name_table');
+    }
+}
